8fd3635010569fe5b78d5df96b69df6efde3f839
Shola Amoo <olayiwolaseun34@gmail.com>
2022-12-18 04:52:53 +0100

    updates

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-// JavaScript code
-console.log("Hello Java")
-console.log(Hello)
+// // JavaScript code
+// console.log("Hello Java")
+// console.log(Hello)
+//API KEY=6f485983f96b59b579c8b243eff19c65
